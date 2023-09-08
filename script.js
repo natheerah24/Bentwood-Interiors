5d86396ad7cb7792765d352a1a768614df7f89c9
natheerah24 <natheerah24@gmail.com>
2023-09-08 13:51:11 +0200

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
-
-		items.forEach((el) => {
-			const minPerSlide = 4
-			let next = el.nextElementSibling
-			for (var i=1; i<minPerSlide; i++) {
-				if (!next) {
-            // wrap carousel by using first child
-            next = items[0]
-        }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
+// function to show image when we click on a image
+function showImage(imageSrc) {
+    let popupImage = document.getElementById("popupImage");
+    popupImage.src = imageSrc;
+     
+    let imagePopup = document.getElementById("imagePopup");
+    imagePopup.style.display = "block";
+    document.body.style.overflow = "hidden";
+  }
+  // function to hide the image when we click on cross button
+  function closeImage() {
+    let imagePopup = document.getElementById("imagePopup");
+    imagePopup.style.display = "none";
+   document.body.style.overflow = "auto";
+  }
